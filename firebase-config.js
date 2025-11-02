32be068a5f78d1433cf8c0189e4c8eeda3b08d3c
emihysenaj <emihysenaj@gmail.com>
2025-11-02 13:38:32 +0100

    Integrate Firebase Authentication in config
    Added Firebase Authentication to the configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,8 @@
 <script type="module">
-  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyCjH7QD6NhDWWOPedKWvdG5V5HG6GnnbH8",
     authDomain: "glospot.firebaseapp.com",
@@ -17,7 +13,8 @@
     measurementId: "G-87VPZLYDKK"
   };
 
-  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+
+  export const auth = getAuth(app);
 </script>
