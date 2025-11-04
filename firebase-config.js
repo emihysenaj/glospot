53dba9e038f3a2fb49ce3665223cb6ea589abb1e
emihysenaj <emihysenaj@gmail.com>
2025-11-04 13:41:59 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCjH7QD6NhDWWOPedKWvdG5V5HG6GnnbH8",
@@ -12,7 +8,3 @@ const firebaseConfig = {
   appId: "1:5569788371:web:b0b9862f36be996c4a1726",
   measurementId: "G-87VPZLYDKK"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
