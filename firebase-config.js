70d3b29cc42345b2c865a6ca525b2eba531819e7
emihysenaj <emihysenaj@gmail.com>
2025-11-04 15:15:28 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,15 @@
-// Import Firebase modules
+// Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
-// Firebase configuration
+// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDnOLBrMG-WBsEhAjRDYKVb897JUqa2zDE",
-  authDomain: "glospot-eed75.firebaseapp.com",
-  projectId: "glospot-eed75",
-  storageBucket: "glospot-eed75.appspot.com",
-  messagingSenderId: "235809233045",
-  appId: "1:235809233045:web:f0ab9a813df1e286c2e39c",
-  measurementId: "G-1VCH03DH82"
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_ID",
+  appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
