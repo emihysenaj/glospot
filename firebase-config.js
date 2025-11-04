16684465548bf841fbc34914daf1ea5207f229cd
emihysenaj <emihysenaj@gmail.com>
2025-11-04 16:05:38 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDnOLBrMG-WBsEhAjRDYKVb897JUqa2zDE",
   authDomain: "glospot-eed75.firebaseapp.com",
   projectId: "glospot-eed75",
-  storageBucket: "glospot-eed75.firebasestorage.app",
+  storageBucket: "glospot-eed75.appspot.com",
   messagingSenderId: "235809233045",
   appId: "1:235809233045:web:f0ab9a813df1e286c2e39c",
   measurementId: "G-1VCH03DH82"
@@ -18,4 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
