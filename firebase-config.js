fdb4558547c05cdbf6c9da0e3b2f528bb9f22842
emihysenaj <emihysenaj@gmail.com>
2025-11-04 14:29:32 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,9 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Make sure to include Firebase scripts in your HTML before this file
+// <script src="https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js"></script>
+
 const firebaseConfig = {
   apiKey: "AIzaSyDnOLBrMG-WBsEhAjRDYKVb897JUqa2zDE",
   authDomain: "glospot-eed75.firebaseapp.com",
@@ -19,7 +14,11 @@ const firebaseConfig = {
   measurementId: "G-1VCH03DH82"
 };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth
+const auth = firebase.auth();
+
+// Expose auth globally
+window.auth = auth;
