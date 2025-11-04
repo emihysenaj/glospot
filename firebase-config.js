b79f62c0c4abee769ed401b5a45dd644602febd9
emihysenaj <emihysenaj@gmail.com>
2025-11-04 15:05:05 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,18 @@
-// firebase-config.js
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
-// Make sure to include Firebase scripts in your HTML before this file
-// <script src="https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js"></script>
-// <script src="https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js"></script>
-
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDnOLBrMG-WBsEhAjRDYKVb897JUqa2zDE",
   authDomain: "glospot-eed75.firebaseapp.com",
   projectId: "glospot-eed75",
-  storageBucket: "glospot-eed75.firebasestorage.app",
+  storageBucket: "glospot-eed75.appspot.com",
   messagingSenderId: "235809233045",
   appId: "1:235809233045:web:f0ab9a813df1e286c2e39c",
   measurementId: "G-1VCH03DH82"
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-
-// Initialize Firebase Auth
-const auth = firebase.auth();
-
-// Expose auth globally
-window.auth = auth;
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
