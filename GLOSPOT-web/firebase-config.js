e0a08b1d0e286703291557b1075e7db872eb8591
emihysenaj <emihysenaj@gmail.com>
2025-11-08 00:24:57 +0100

    Add files via upload

---
+++ GLOSPOT-web/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnOLBrMG-WBsEhAjRDYKVb897JUqa2zDE",
+  authDomain: "glospot-eed75.firebaseapp.com",
+  projectId: "glospot-eed75",
+  storageBucket: "glospot-eed75.appspot.com",
+  messagingSenderId: "235809233045",
+  appId: "1:235809233045:web:f0ab9a813df1e286c2e39c",
+  measurementId: "G-1VCH03DH82"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
